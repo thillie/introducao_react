1dff308cf5dc1a090a899123d6dcc4342008887e
Thilliê Cristina Moreira de Souza <thillie.moreira@gmail.com>
2024-06-23 21:27:12 -0300

    Blog Pessoal implementando React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://images.app.goo.gl/z6QSTwRBJCqvGYvv9" alt="Imagem Tela Inicial" className="img"/>
+           
+        </>
+    );
+}
+
+export default Home;
